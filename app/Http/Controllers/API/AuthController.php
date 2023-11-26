8cea82ea0178a6d8f19c05ee53597433a838ddde
Abdullah Al Nahian <info.nahian13@gmail.com>
2023-11-26 20:17:45 +0600

    user login feature

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,21 +6,43 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
 
-    public function login(LoginRequest $request){
-        return response()->json('login');
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $credentials = $request->only('email', 'password');
+        if(Auth::attempt($credentials)){
+            $user = Auth::user();
+            $token = $user->createToken('accessToken')->accessToken;
+
+            return response()->json([
+                'status'=> 'true',
+                'data'  => [
+                    'message'   => 'Login successfully',
+                    'token'     => $token,
+                    'user'      => $user
+                ]
+            ]);
+        }
+        return response()->json([
+            'message'  => 'Wrong Credentials'
+        ], 401);
     }
 
-    public function register(RegisterRequest $request){
-
+    public function register(RegisterRequest $request): JsonResponse
+    {
         $user = User::create($request->all());
         return response()->json([
+            'status'=> 'true',
             'message'   => 'User registration successfull',
-            'user'      => $user
+                'data'  => [
+                    'user'      => $user
+                ]
         ]);
     }
 }
