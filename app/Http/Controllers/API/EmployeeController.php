3e1a4facd0bbd6d7afd74ae7ccc96fc4f8429586
Abdullah Al Nahian <info.nahian13@gmail.com>
2023-11-29 01:03:40 +0600

    employee add feature updated

--- app/Http/Controllers/API/EmployeeController.php
+++ app/Http/Controllers/API/EmployeeController.php
@@ -16,10 +16,10 @@ class EmployeeController extends Controller
         return $this->apiSuccessResponse('Employee list', $employee);
     }
 
-    public function store (EmployeeRequest $request): JsonResponse
+    public function store (EmployeeRequest $request)
     {
         $employee = Employee::create($request->validated());
-        $employeeIds = Employee::query()->pluck('id');
+        $employeeIds = $request->achievement_id;
         $employee->achievement()->attach($employeeIds);
         
         return $this->apiSuccessResponse('Employee is created', $employee);
