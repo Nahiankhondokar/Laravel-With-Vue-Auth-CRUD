a572b265692f6d7bb29b2e50dcf65d9e6279d8b5
Abdullah Al Nahian <info.nahian13@gmail.com>
2023-12-04 19:09:07 +0600

    user seeder modified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\Department::factory(10)->create();
-        \App\Models\User::factory(2)->create();
+        \App\Models\User::factory(1)->create();
         $this->call([
             AchievementSeeder::class,
             EmployeeSeeder::class,
